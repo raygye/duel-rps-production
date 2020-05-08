48adad4193f873f9989a8801d4730886490b01e8
yeG10 <garyye2007@gmail.com>
2020-05-08 00:38:21 -0400

    Initial commit

---
+++ precache-manifest.dc5d2a8c36803c0f2b86b7772f4412d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0a39d0fc8adc6e0c80868f12af2b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e28c03b0bd9a1c56292",
+    "url": "/static/css/main.b742fddf.chunk.css"
+  },
+  {
+    "revision": "fc026b26203335afb63d",
+    "url": "/static/js/2.4e498c24.chunk.js"
+  },
+  {
+    "revision": "a360305518243deee3bfebd4256d9edd",
+    "url": "/static/js/2.4e498c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e28c03b0bd9a1c56292",
+    "url": "/static/js/main.e54853e5.chunk.js"
+  },
+  {
+    "revision": "4940aa85eab74602c546",
+    "url": "/static/js/runtime-main.c0563e9a.js"
+  },
+  {
+    "revision": "8c1ed6aa973434d7f8b18a46dfbed648",
+    "url": "/static/media/duel.8c1ed6aa.png"
+  },
+  {
+    "revision": "676924caa1ef631ab3715d4659a8d12d",
+    "url": "/static/media/paper.676924ca.png"
+  }
+]);
