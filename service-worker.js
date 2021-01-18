7cec40ab69a7e850d1e37665576b479b3c32686f
yeG10 <garyye2007@gmail.com>
2021-01-18 18:14:50 -0500

    ngrok

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd4575431afb6e258b7a12291bed0575.js"
+  "/precache-manifest.99ea9697e54a1699728e1201c7e8b452.js"
 );
 
 self.addEventListener('message', (event) => {
